2224a175faabd86b29161d684132292b302e12e4
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-10-04 18:54:13 +0530

    Add files via upload

---
+++ #001-singlylinkedlist.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+ #include<stdlib.h>
+ struct Node{
+ int data;
+ struct Node *next;
+ };
+ struct Node *head;
+ void insert(int x){
+ struct Node *link = (struct Node*)malloc(sizeof(struct Node));
+ link->data = x;
+ link->next = head;
+ head = link;
+ }
+ void disp(){
+ struct Node *prt = head;
+ printf("List is : \n");
+ while(prt!=NULL){
+ printf(" %d",prt->data);
+ prt = prt->next;
+ }
+ printf("\n");
+ }
+ 
+ int main(){
+ insert(10);
+ insert(20);
+ insert(30);
+ disp();
+ return 0;
+ }
