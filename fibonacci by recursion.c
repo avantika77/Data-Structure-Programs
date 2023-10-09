e4fc80d5f4cdb5d075b966e56c567231bfbf83a7
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-10-09 22:15:02 +0530

    Add files via upload

---
+++ fibonacci by recursion.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+// Function to calculate the nth Fibonacci number using recursion
+long long int fibonacci(int n) {
+    if (n <= 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of terms in the Fibonacci series: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    for (i = 0; i < n; i++) {
+        printf("%lld", fibonacci(i));
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
+
